00a51835c5b4705c3583f8362bcb0f42c8402bbb
Ram Chaithanya <ramchaithanya1999@.com>
2025-07-21 14:58:35 +0530

    Test Shell Scripting

--- SpecialVariables.sh
+++ SpecialVariables.sh
@@ -3,4 +3,10 @@
 echo "All variables :$@"
 echo "No of variables Passed :$#"
 echo "script Name :$0"
-echo "Current working Directory :$PWD
+echo "Current working Directory :$PWD"
+echo "home directory of current user :$HOME"
+echo "which user running :$USER"
+echo "Host Name :$HOSTNAME"
+echo "Process id of current shell script :$$"
+sleep 60
+echo "Process ID of Last background command :$!"
