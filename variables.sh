88f7abb1192db5b329149782a6df1de05c3de200
Ram Chaithanya <ramchaithanya1999@.com>
2025-07-21 11:53:22 +0530

    Test Shell Scripting

---
+++ variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+Person1=Ramesh
+Person2=Suresh
+
+echo "$Person1:: Hello $Person2,How are You"
+echo "$Person2:: Yes, I'm Good what about You"
+echo "$Person1:: Yeah I too, How is going your work?"
+echo "$Person2:: Yes $Person1, It going very i want to upgrade my skill with DevOps side"
+echo "$Person1:: I'm already working in DevOps
