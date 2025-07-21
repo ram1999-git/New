a3559e91820770866db74c67af8de6b514210aae
Ram Chaithanya <ramchaithanya1999@.com>
2025-07-21 14:19:01 +0530

    Test Shell Scripting

--- datatypes.sh
+++ datatypes.sh
@@ -5,11 +5,12 @@ Number1=$1
 Number2=$2
 
 sum=$(($Number1 + $Number2))
+echo "Sum of $Number1 and $Number2 is: $sum"
 sub=$(($Number1 - $Number2))
+echo "Subtraction of $Number1 and $Number2 is: $sub"
 mult=$(($Number1 * $Number2))
+echo "Multiplication of $Number1 and $Number2 is: $mult"
 div=$(($Number1 / $Number2))
+echo "Division of $Number1 and $Number2 is: $div"
 
-echo "Sum: $sum"
-echo "Subtraction: $sub"
-echo "Multiplication: $mult"
-echo "Division: $div"
+
