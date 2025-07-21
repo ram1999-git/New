5cf6b8c0c7ee46c91fe6e8d2a9deea5950c1c38d
Ram Chaithanya <ramchaithanya1999@.com>
2025-07-21 12:04:58 +0530

    Test Shell Scripting

---
+++ Arguments and Parameters.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#Declaring variables 
+Person1=$1
+Person2=$2
+
+# Reffereing Variables
+echo "$Person1:: Hello $Person2,How are You"
+echo "$Person2:: Yes, I'm Good what about You"
+echo "$Person1:: Yeah I too, How is going your work?"
+echo "$Person2:: Yes $Person1, It going very i want to upgrade my skill with DevOps side"
+echo "$Person1:: I'm already working in DevOps
