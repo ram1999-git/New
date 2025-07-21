f8ef2c71c0f3f9f5dee299f97cfa36361ab27f30
Ram Chaithanya <ramchaithanya1999@.com>
2025-07-21 12:46:59 +0530

    Test Shell Scripting

--- datatypes.sh
+++ datatypes.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 #arthimatic operation of 2 numbers
 
+
 Number1=$1
 Number2=$2
 
-sum=$(($Number1+$Number2))
-SUb=$(($Number1-$Number2))
-Mult=$(($Number1*$Number2))
-Div=$(($Number1/$Number2))
+sum=$(($Number1 + $Number2))
+sub=$(($Number1 - $Number2))
+mult=$(($Number1 * $Number2))
+div=$(($Number1 / $Number2))
 
 echo "Sum: $sum"
-echo "Subtraction: $Sub"
-echo "Multiplication: $Mult"
-echo "Division: $Div"
+echo "Subtraction: $sub"
+echo "Multiplication: $mult"
+echo "Division: $div"
