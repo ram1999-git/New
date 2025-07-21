488b35fa4e3c7ce24d65218bea612bcd554d77d5
Ram Chaithanya <ramchaithanya1999@.com>
2025-07-21 15:40:56 +0530

    Test Shell Scripting

--- conditions.sh
+++ conditions.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-NUMBER= $1
 
-IF [ $NUMBER -gt 10 ]
-then 
-echo "Given $NUMBER is greater than 10"
+NUMBER=$1
+
+if [ $NUMBER -gt 10 ]; then
+    echo "Given $NUMBER is greater than 10"
 else
-echo ""Given $NUMBER is leser than 10"
+    echo "Given $NUMBER is lesser than 10"
 fi
