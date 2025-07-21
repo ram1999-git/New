4a91de743acd54f60bd569534dc7cdb48e8d37df
Ram Chaithanya <ramchaithanya1999@.com>
2025-07-21 12:40:16 +0530

    Test Shell Scripting

---
+++ datatypes.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#arthimatic operation of 2 numbers
+
+Number1=$1
+Number2=$2
+
+sum=(($Number1+$Number2))
+SUb=(($Number1-$Number2))
+Mult=(($Number1*$Number2))
+Div=(($Number1/$Number2))
