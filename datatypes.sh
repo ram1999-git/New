d8efec132939e8ebff8d5fb8135f42e698e3029f
Ram Chaithanya <ramchaithanya1999@.com>
2025-07-21 12:42:05 +0530

    Test Shell Scripting

--- datatypes.sh
+++ datatypes.sh
@@ -4,7 +4,7 @@
 Number1=$1
 Number2=$2
 
-sum=(($Number1+$Number2))
-SUb=(($Number1-$Number2))
-Mult=(($Number1*$Number2))
-Div=(($Number1/$Number2))
+sum=$(($Number1+$Number2))
+SUb=$(($Number1-$Number2))
+Mult=$(($Number1*$Number2))
+Div=$(($Number1/$Number2))
